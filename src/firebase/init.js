813eb851166f1e6655eb595725dffa746fbad370
Aron Tolentino <arontolentino@outlook.com>
2019-07-28 18:12:13 -0400

    Integrate function to check if user alias exists

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,14 +1,14 @@
-import firebase from "firebase";
-import firestore from "firebase/firestore";
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCytPqACauL7988AuF7eRueK1ppG88yPBA",
-  authDomain: "geo-chat-dfae7.firebaseapp.com",
-  databaseURL: "https://geo-chat-dfae7.firebaseio.com",
-  projectId: "geo-chat-dfae7",
-  storageBucket: "",
-  messagingSenderId: "1086360932594",
-  appId: "1:1086360932594:web:f92887a7c03fe760"
+  apiKey: 'AIzaSyCytPqACauL7988AuF7eRueK1ppG88yPBA',
+  authDomain: 'geo-chat-dfae7.firebaseapp.com',
+  databaseURL: 'https://geo-chat-dfae7.firebaseio.com',
+  projectId: 'geo-chat-dfae7',
+  storageBucket: '',
+  messagingSenderId: '1086360932594',
+  appId: '1:1086360932594:web:f92887a7c03fe760'
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -14,22 +14,56 @@
         <label for="alias">Alias:</label>
         <input type="text" name="alias" v-model="alias" />
       </div>
+      <p class="red-text center" v-if="feedback">{{ feedback }}</p>
       <div class="field center">
-        <button class="btn deep-purple">Signup</button>
+        <button class="btn deep-purple" @click.prevent="signup">Signup</button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import slugify from 'slugify';
+import db from './../firebase/init.js';
+
 export default {
   name: 'Signup',
   data: function() {
     return {
       email: null,
       password: null,
-      alias: null
+      alias: null,
+      feedback: null,
+      slug: null
     };
+  },
+  methods: {
+    signup() {
+      if (this.alias) {
+        this.slug = slugify(this.alias, {
+          replacement: '-',
+          remove: /[*+~.()'"!:@]/g,
+          lower: true
+        });
+        let ref = db.collection('users').doc(this.slug);
+        ref
+          .get()
+          .then(doc => {
+            console.log(doc);
+            if (doc.exists) {
+              this.feedback = 'This alias already exists';
+            } else {
+              this.feedback = 'This alias is free to use';
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        console.log(this.slug);
+      } else {
+        this.feedback = 'You must enter an alias';
+      }
+    }
   }
 };
 </script>
